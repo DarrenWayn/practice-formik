e7abb2ab31dc2c792208f7329d538a8bfc04fbcd
Darren Wayn <dwayn.dev@gmail.com>
2023-01-19 19:20:01 +0700

    refactor formik

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import YoutubeForm from "./components/YoutubeForm";
+/* import OldYoutubeForm from "./components/OldYoutubeForm"; */
 
 function App() {
   return (
     <div className="App">
       <YoutubeForm />
+      {/* <OldYoutubeForm /> */}
     </div>
   );
 }
